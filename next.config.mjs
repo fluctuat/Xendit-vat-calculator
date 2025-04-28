4d67d767096cee5fc9c631ef0621a43aa4680f56
Vincent <vincent.debast@xendit.co>
2025-04-28 17:31:42 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://xendit-docs.document360.io https://docs.xendit.co",
+          },
+        ],
+      },
+    ];
+  },
 }
 
 export default nextConfig
